6a3198071769b05c9b84daed78b9e0b3f45a7ed3
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-07-20 13:58:32 +0300

    Addend field with last movies!

--- MovieCatalog/movies/templatetags/menu_tags.py
+++ MovieCatalog/movies/templatetags/menu_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from movies.models import Category
+from movies.models import Category, Movie
 
 
 register = template.Library()
@@ -9,3 +9,10 @@ def get_category_drop_down():
     """Рендер всплывающего меню с категориями в меню"""
     categories = Category.objects.all()
     return {'categories': categories}
+
+
+@register.inclusion_tag('include/tags/last_movies.html')
+def get_last_movies():
+    """Рендер поля с последними добавленными фильмами"""
+    last_movies = Movie.objects.all().order_by('-year')[:5]
+    return {'movies': last_movies}
